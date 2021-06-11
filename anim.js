fe6fd2770638e97a3857f5bd80e6027f911e9a7f
Berrachedi-yousra <58655560+Berrachedi-yousra@users.noreply.github.com>
2021-06-12 00:52:21 +0100

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,23 @@
+function godossier(){
+    onclick=window.location.href='medecin.html';
+}
+
+function gochercher(){
+    onclick=window.location.href='liste.html';
+}
+
+function gocalendrier(){
+    onclick=window.location.href='calendrier.html';
+}
+
+function gotrait(){
+    onclick=window.location.href='trait.html';
+}
+
+function goalerte(){
+    onclick=window.location.href='alerte.html';
+}
+
+function seconnecter(){
+    onclick=window.location.href='trait.html';
+}
